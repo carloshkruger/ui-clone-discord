0dccf91f1e24790427ee28b279f4c455b8d6a045
Carlos Henrique Kruger <carloshkruger@gmail.com>
2020-08-17 17:35:46 -0300

    first commit

---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,48 @@
+import React, { useRef, useEffect } from 'react';
+
+import ChannelMessage, { Mention } from '../ChannelMessage';
+
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
+
+const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef]);
+
+  return (
+    <Container>
+      <Messages ref={messagesRef}>
+        <ChannelMessage
+          author="Carlos"
+          date="01/01/2000"
+          content="Mensagem de teste"
+        />
+
+        <ChannelMessage
+          author="Diego"
+          date="01/01/2000"
+          content={
+            <>
+              <Mention>@Carlos</Mention>, mensagem do Diego
+            </>
+          }
+          hasMention
+          isBot
+        />
+      </Messages>
+
+      <InputWrapper>
+        <Input type="text" placeholder="Conversar em #chat-livre" />
+        <InputIcon />
+      </InputWrapper>
+    </Container>
+  );
+}
+
+export default ChannelData;
---
+++ src/components/UserList/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import { Container, Role, User, Avatar } from './styles';
+
+interface UserProps {
+  nickname: string;
+  isBot?: boolean;
+}
+
+const UserRow: React.FC<UserProps> = ({
+  nickname,
+  isBot
+}) => {
+  return (
+    <User>
+      <Avatar className={isBot ? 'bot' : ''} />
+
+      <strong>{nickname}</strong>
+
+      { isBot && <span>Bot</span> }
+    </User>
+  )
+};
+
+const UserList: React.FC = () => {
+  return (
+    <Container>
+      <Role>Disponível - 1</Role>
+      <UserRow nickname="Carlos" />
+
+      <Role>Offline - 10</Role>
+      <UserRow nickname="Diego" isBot />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+      <UserRow nickname="Diego" />
+    </Container>
+  );
+}
+
+export default UserList;
